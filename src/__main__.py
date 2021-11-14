3aefb1efb5e3de791dc1c7cfdfbeb6763417313c
Synim Selimi <26981059+SynimSelimi@users.noreply.github.com>
2021-11-14 10:47:10 +0100

    Add instances and main parsing

--- src/__main__.py
+++ src/__main__.py
@@ -1,2 +1,62 @@
+from helpers import *
+
+"""
+Parsing method -
+This method contains the main instance parsing logic
+"""
+def parse(filepath = None):
+    filepath = filepath or get_filepath()
+    data = read_file(filepath)
+    if not data: return None
+
+    return data
+
+def save_solution(filepath, data):
+    folder = 'solutions'
+    filename = os.path.basename(filepath)
+    save_file(f'{folder}/SOLUTION-{filename}', data, folder)
+
+"""
+Process method -
+This method contains the main instance processing and modeling logic
+"""
+def process(data):
+    if not data: return None
+
+    courses, periods, \
+    slots_per_day, teachers, \
+    constraints, rooms, curricula, \
+    primary_primary_distance = \
+    pluck(data, 
+        'Courses', 'Periods', 'SlotsPerDay', 
+        'Teachers', 'Constraints', 'Rooms', 
+        'Curricula', 'PrimaryPrimaryDistance'
+    )
+
+    # DO SOMETHING WITH THE DATA
+    # THEN RETURN THE PROCESSED DATA
+    processed_data = data
+    return data
+
+def save_solution(filepath, data):
+    folder = 'solutions'
+    filename = os.path.basename(filepath)
+    save_file(f'{folder}/SOLUTION-{filename}', data, folder)
+
+
+"""
+Main program -
+This section runs the solution
+"""
+def main():
+    print("Running solver on instance:", get_filepath())
+    data = process(parse())
+    save_solution(get_filepath(), data)
+    print("Solver completed.")
+    print("Solution instances are in solutions.")
+
+"""
+Execution
+"""
 if __name__ == '__main__':
-    print("ETT solver is running...")
+    main()
---
+++ src/helpers.py
@@ -0,0 +1,63 @@
+import json
+import os
+import sys
+import imp
+import string
+import random
+from pathlib import Path
+
+try:
+    imp.find_module('names')
+    import names
+    names_imported = True
+except ImportError:
+    names_imported = False
+
+"""
+Helper methods -
+This section contains all helper methods, which aid in 
+file manipulation, enumeration, system os paths, etc.
+"""
+
+def room2supervisors(enum):
+    if not enum: return None
+    return {
+        "Small": 1,
+        "Medium": 2,
+        "Large": 3
+    }[enum]
+
+def read_file(filepath):
+    try:
+        with open(filepath) as file:
+            data = json.load(file)
+        return data
+    except:
+        print("Cannot open file! Might not exist.")
+        return None
+
+def save_file(filepath, data, folder = 'modified_instances'):
+    Path(folder).mkdir(parents=True, exist_ok=True)
+    try:
+        with open(filepath, 'w+', encoding='utf-8') as f:
+            json.dump(data, f, ensure_ascii=False, indent=2)
+        return True
+    except:
+        print("Cannot write to file!")
+        return False
+
+def get_filepath(default_file = 'instances/D1-1-16.json'):
+    if len(sys.argv) > 1: return sys.argv[1]
+    return default_file
+
+pluck = lambda dict, *args: (dict.get(arg) for arg in args)
+
+def random_name():
+    if names_imported: return names.get_first_name()
+    letters = string.ascii_letters
+    return ''.join(random.choice(letters) for i in range(random.randrange(25)))
+
+def random_range(threshold = 20, size = 5):
+    random_size = random.randint(0, size)
+    threshold_range = range(1, threshold)
+    return random.sample(threshold_range, random_size)
